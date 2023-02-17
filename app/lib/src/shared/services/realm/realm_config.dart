ed0aea0e7762ed328a9401009327b71e1207bc57
Felipe Tércio <fatercio@gmail.com>
2023-02-16 23:22:42 -0300

    Add models tasks and taskboards into realm

--- app/lib/src/shared/services/realm/realm_config.dart
+++ app/lib/src/shared/services/realm/realm_config.dart
@@ -1,12 +1,10 @@
 import 'package:app/src/shared/services/realm/models/configuration_model.dart';
+import 'package:app/src/shared/services/realm/models/task_model.dart';
 import 'package:realm/realm.dart';
 
 LocalConfiguration config = Configuration.local(
-  [
-    ConfigurationModel.schema,
-  ],
+  [ConfigurationModel.schema, Task.schema, TaskBoard.schema],
   initialDataCallback: (realm) {
     realm.add(ConfigurationModel('system'));
   },
-  //path: './realm/realm.db',
 );
